da30c12670b13cca4ebd5737ecd76694fafa83e6
Aniruddha Deshpande <aniruddha.d7@gmail.com>
2026-06-08 14:28:33 +0530

    Phase 1: Module Skeleton completed

--- magento/app/etc/config.php
+++ magento/app/etc/config.php
@@ -386,6 +386,7 @@ return [
         'PayPal_BraintreeGiftCardAccount' => 1,
         'PayPal_BraintreeGiftWrapping' => 1,
         'PayPal_BraintreeGraphQl' => 1,
-        'PayPal_BraintreeReward' => 1
+        'PayPal_BraintreeReward' => 1,
+        'Training_StockNotifyQueue' => 1
     ]
 ];
